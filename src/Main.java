690d1a36c93707a2b054795c8e2d8b1d0458923c
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-21 15:17:27 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("click 1 to enter data");
+        System.out.println("click 2 to view data");
+        System.out.println("click 3 to search data");
+        System.out.println("click 4 to delete data");
+        System.out.println("click 5 to exit");
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+    }
+}
